822af5a71340c09a122a22da5b29dcb425db9490
Sami Abduljalil <samialkindi0708@gmail.com>
2015-11-02 00:08:46 +0100

    Update flickr.php

--- flickr.php
+++ flickr.php
@@ -6,9 +6,9 @@ set_time_limit(0);
 
 class Flickr {
 
-	private $apiKey = '9a7d65ff0d5468de279ef2fc2601a115';
+	private $apiKey = '';
 
-    	private $secretKey = '138e36fe069ba6e0';
+    	private $secretKey = '';
 
 	public function __construct() {
 
